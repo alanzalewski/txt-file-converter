6327df8e76c1728fdfc970ca986007232bd5e18d
Alan Zalewski <84343976+alanzalewski@users.noreply.github.com>
2021-06-04 10:44:10 -0400

    Add files via upload

---
+++ convertfiles.py
@@ -0,0 +1,66 @@
+'''This program opens a window that allows the user to select txt files and convert them to either xlsx or csv format, outputting them to the same file.'''
+
+import tkinter as tk
+from tkinter import filedialog
+import pandas as pd
+import os
+
+root = tk.Tk()
+
+canvas1 = tk.Canvas(root, width=250, height=110, bg='white')
+canvas1.pack()
+
+def convertToXlsx():
+    # allows user to select txt files for conversion
+    import_file_path = filedialog.askopenfilenames()
+    
+    # loops through each selected file
+    for entry in import_file_path:
+        export_file_path = entry.replace('.txt','.xlsx')
+    
+        with open(entry,'r') as txtfile:
+	    # formatting data
+            rawdata = [row.split('\t') for row in txtfile]
+            r=0
+            for row in rawdata:
+                if r<len(rawdata)-1:
+                    row[-1] = row[-1][0:-1]
+                r+=1
+        
+            # writing to xlsx file, leaving only sampling frequency in header
+            df = pd.DataFrame(rawdata[4:])
+            df.loc[3:len(rawdata),0:8] = df.loc[3:len(rawdata),0:8].astype(float)
+            excelfile = pd.ExcelWriter(export_file_path, engine='xlsxwriter')
+            df.to_excel(excelfile, sheet_name='Sheet1', index=False, header=False)
+            excelfile.save()
+            excelfile.close()
+    
+def convertToCsv():
+    # allows user to select txt files for conversion
+    import_file_path = filedialog.askopenfilenames()
+    
+    # loops through each selected file
+    for entry in import_file_path:
+        export_file_path = entry.replace('.txt','.csv')
+        
+        with open(entry,'r') as txtfile:
+	    # formatting data
+            rawdata = [row.split('\t') for row in txtfile]
+            r=0
+            for row in rawdata:
+                if r<len(rawdata)-1:
+                    row[-1] = row[-1][0:-1]
+                r+=1
+            
+            # writing to csv file, leaving only sampling frequency in header
+            df = pd.DataFrame(rawdata[4:])
+            df.loc[3:len(rawdata),0:8] = df.loc[3:len(rawdata),0:8].astype(float)
+            df.to_csv(export_file_path, index=False, header=False)
+    
+saveAsButtonXlsx = tk.Button(text='Select files to convert to xlsx', command=convertToXlsx, bg='gainsboro', fg='black', font=('helvetica',10))
+canvas1.create_window(125,35,window=saveAsButtonXlsx)
+
+saveAsButtonCsv = tk.Button(text='Select files to convert to csv', command=convertToCsv, bg='gainsboro', fg='black', font=('helvetica',10))
+canvas1.create_window(125,80,window=saveAsButtonCsv)
+
+root.mainloop()
